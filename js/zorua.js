c849bf9448a8eb1794c9614ab3785ec57f58d78c
ZoruaSama <252339319@qq.com>
2020-08-03 17:32:28 +0800

    Site updated: 2020-08-03 17:32:14

--- js/zorua.js
+++ js/zorua.js
@@ -1,5 +1,6 @@
+var zoruapswd="00000";
 jQuery(document).on('copy', function(e)
-	{
+	{ if (zoruapswd!="zorua"){
 	  var selected = window.getSelection();
 	  var selectedText = selected.toString().replace(/\n/g, '<br>');  // Solve the line breaks conversion issue
 	  var copyFooter = '<br>---------------------<br>著作权归作者所有。<br>' 
@@ -17,32 +18,9 @@ jQuery(document).on('copy', function(e)
 	  selected.selectAllChildren( copyHolder[0] );
 	  window.setTimeout(function() {
 	      copyHolder.remove();
-	  },0);
+	  },0);}
 	});
 
-
-var card_category_list = document.getElementsByClassName(
-    "card-category-list child"
-);
-var item = document.getElementsByClassName("card-category-list-item");
-function toggle(t) {
-    var display = t.parentElement.nextSibling.style.display;
-    if (display == "none") {
-        t.parentElement.nextSibling.style.display = "block";
-        t.parentElement.nextSibling.style.height = "100%";
-        t.className = t.className.replace("fa-chevron-up", "fa-chevron-down");
-    } else {
-        t.parentElement.nextSibling.style.display = "none";
-        t.className = t.className.replace("fa-chevron-down", "fa-chevron-up");
-    }
-}
-
-for (var i = 0; i < card_category_list.length; i++) {
-    card_category_list[i].style.display = "none";
-    card_category_list[i].style.transition = "all 1s";
-    card_category_list[i].previousSibling.innerHTML +=
-        '<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" onclick="toggle(this)"></i>';
-}
 
 
 /* eslint-disable */
